8c7b02fd97eab2597efc508e26a454c4c49a2783
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-04 20:31:08 -0400

    Contact y About Page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def about_us; end
+
+  def contact_us; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'landings#index'
+
+  get '/about_us', to: 'static_pages#about_us', as: :about_us
+  get '/contact_us', to: 'static_pages#contact_us', as: :contact_us
 end
