a5e462f7734ff4f9c450d34c7f04ebee02132da7
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-06 16:21:36 -0400

    Adding Restaurants first Actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,25 @@
 class RestaurantsController < ApplicationController
-  def index; end
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params(params[:restaurant]))
+    @restaurant.save
+    redirect_to @restaurant
+  end
+
+  def show; end
+
+  private
+
+  def restaurant_params(params)
+    params.permit(:name, :description, :address1,
+                  :address2, :city, :state,
+                  :zipcode, :phone, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants
   root to: 'landings#index'
 
   get '/about_us', to: 'static_pages#about_us', as: :about_us
