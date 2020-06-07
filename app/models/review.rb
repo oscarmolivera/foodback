6f42c192e966fc64b145825e3dfa8aade339923b
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-07 19:35:43 -0400

    Relation and Avg_Rating Methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,20 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  belongs_to :user
+
+  scope :ratings, ->(restaurant) { where('restaurant_id = ?', restaurant) }
+
+  def self.avg_rating(restaurant)
+    return 0 if ratings(restaurant).count.zero? || stars_per_restaurant(restaurant).zero?
+
+    stars_per_restaurant(restaurant) / reviews_of_restaurant(restaurant).round(2)
+  end
+
+  def self.reviews_of_restaurant(restaurant)
+    ratings(restaurant).count
+  end
+
+  def self.stars_per_restaurant(restaurant)
+    ratings(restaurant).map(&:rating).reduce(:+)
+  end
 end
