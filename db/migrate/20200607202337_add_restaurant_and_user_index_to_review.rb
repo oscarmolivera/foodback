9f5e7682ffc265a5c20b94ff4e5dec370fb9eedb
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-07 16:55:45 -0400

    Adding Reviews References(restaurant and users)

---
+++ db/migrate/20200607202337_add_restaurant_and_user_index_to_review.rb
@@ -0,0 +1,6 @@
+class AddRestaurantAndUserIndexToReview < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :reviews, :restaurant, null: false, foreign_key: true
+    add_reference :reviews, :user, null: false, foreign_key: true
+  end
+end
