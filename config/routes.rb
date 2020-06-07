29ca28485243f9250dca0050cc27f6fc1c862849
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-07 16:57:02 -0400

    nesting Review Routes into Resttaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :restaurants
+
+  resources :restaurants do
+    resources :reviews, except: %i[index show]
+  end
+
   root to: 'landings#index'
 
   get '/about_us', to: 'static_pages#about_us', as: :about_us
