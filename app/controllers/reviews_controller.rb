c81b6d8e714af28aa41ac3714824f2272db12c4d
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-07 19:33:32 -0400

    Controlador Reviews (New Create)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create new]
+  before_action :set_restaurant, only: %i[create new]
+
+  def new
+    @review = Review.new(restaurant: @restaurant)
+  end
+
+  def create
+    @review = current_user.reviews.build(reviews_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to @restaurant
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
