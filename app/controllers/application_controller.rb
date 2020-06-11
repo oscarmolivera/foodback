3f6338865b103932d3fb7bc129dc0b70a93b5664
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-10 23:25:26 -0400

    New method to accept firstName LastName in Devise Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, only: [:create]
   add_flash_types :info, :warning
+
+  before_action :add_parameters_to_users, if: :devise_controller?
+
+  def add_parameters_to_users
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])
+  end
 end
