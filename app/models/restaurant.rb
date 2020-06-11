44703b5bbfff9d630f38262b2e492365c8376cd2
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-11 17:19:42 -0400

    Methods for Search function

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,4 +10,13 @@ class Restaurant < ApplicationRecord
   def full_address
     [address1, city, state, zipcode].join(', ')
   end
+
+  def self.search(params)
+    restaurants = Restaurant.where(category_id: params[:category].to_i)
+    if params[:search].present?
+      restaurants = restaurants.where('name like ? or description like ?', "%#{params[:search]}%", "%#{params[:search]}%") 
+    end
+    restaurants = restaurants.near(params[:location], 20) if params[:location].present?
+    restaurants
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :restaurants do
     resources :reviews, except: %i[index show]
+    collection do
+      get 'search'
+    end
   end
 
   root to: 'landings#index'
