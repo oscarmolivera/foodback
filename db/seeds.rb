f871727f1a3e0b2dc81bb23a9469b191507ba119
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-11 15:06:49 -0400

    New seed file for GeoCoder Longitude/Latitude

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,14 +32,16 @@ puts '8 Categories Created!'
   Restaurant.create!(
     name: Faker::Restaurant.name,
     description: Faker::Restaurant.description,
-    address1: "#{Faker::Address.street_address} #{Faker::Address.community}",
+    address1: Faker::Address.full_address,
     address2: "#{Faker::Address.street_address} #{Faker::Address.community}",
     city: Faker::Address.city,
     state: Faker::Address.state,
     zipcode: Faker::Address.zip_code,
     phone: Faker::PhoneNumber.phone_number_with_country_code,
     email: Faker::Internet.email,
-    category_id: Faker::Number.between(from: 1, to: 8)
+    category_id: Faker::Number.between(from: 1, to: 8),
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude
   )
 end
 puts '25 Restaurants Created!'
@@ -48,7 +50,7 @@ puts '25 Restaurants Created!'
   3.times do
     Review.create!(
       comment: Faker::Restaurant.review,
-      rating: Faker::Number.between(from: 0, to: 5),
+      rating: Faker::Number.between(from: 1, to: 5),
       created_at: Faker::Date.backward(days: 24),
       updated_at: Date.today,
       restaurant_id: Faker::Number.between(from: 1, to: 25),
@@ -56,4 +58,4 @@ puts '25 Restaurants Created!'
     )
   end
 end
-puts '75 Reviews created! 3 for each restaurant'
+puts '75 Reviews created!'
