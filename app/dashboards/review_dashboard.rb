5c2addef032c5ecae66332130002a652bce22f50
Oscar M Olivera <oscarmolivera@gmail.com>
2020-06-18 22:16:53 -0400

    Aanother N+1 Bullet Gem Correction

--- app/dashboards/review_dashboard.rb
+++ app/dashboards/review_dashboard.rb
@@ -23,8 +23,6 @@ class ReviewDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    restaurant
-    user
     id
     comment
   ].freeze
